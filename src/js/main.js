cefe0a99c4a603cb24e15e02817f7b30c351b66e
Markus Tacker <m@coderbyheart.com>
2017-03-12 23:47:09 +0100

    feat(Main): add main section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 import loadFont from 'meownica-web-fonts-loader'
 
-loadFont('//fonts.googleapis.com/css?family=Roboto:400,700,900', 'webfont-loaded')
+loadFont('//fonts.googleapis.com/css?family=Roboto:300,400,700,900', 'webfont-loaded')
