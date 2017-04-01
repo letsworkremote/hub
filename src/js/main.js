42ca95e09e09f970406845fabb4a13e989e6793a
Markus Tacker <m@coderbyheart.com>
2017-04-01 20:18:56 +0200

    Updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+/* global document */
+
 import loadFont from 'meownica-web-fonts-loader'
 
 loadFont('//fonts.googleapis.com/css?family=Roboto:300,400,700,900', 'webfont-loaded')
+
+const s = document.createElement('script')
+s.src = 'https://lets-work-remote.disqus.com/embed.js'
+s.setAttribute('data-timestamp', +new Date())
+document.body.appendChild(s)
